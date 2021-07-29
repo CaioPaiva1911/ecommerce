0e277ee26643cc2ed6e1bdc6ffee707f32311d0c
Caio Paiva <caiowick@hotmail.com>
2021-07-29 09:27:26 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use Hcode\Page;
-use Hcode\PageAdmin;
+use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -20,6 +22,8 @@ $app->get('/', function() {
 });
 // Rota
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
@@ -27,7 +31,33 @@ $app->get('/admin', function() {
 
 
 });
+// Rota de login
+$app->get('/admin/login', function(){
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+	
+	User::login($_POST["login"], $_POST["password"]); 
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
 
 $app->run();
 
- ?>
+?>
